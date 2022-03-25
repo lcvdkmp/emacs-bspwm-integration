1428cacfe9ca2a2a060d8e7507dc8d75a9877033
Luca van der Kamp <luca@dekabel.org>
2022-03-25 18:34:08 +0100

    Add comments

--- emacs-bspwm-integration.el
+++ emacs-bspwm-integration.el
@@ -2,9 +2,16 @@
 ;; Inspired by https://sqrtminusone.xyz/posts/2021-10-04-emacs-i3/
 
 (defmacro bspc (&rest args)
+  "Convenient macro to call bspc."
      `(start-process "emacs-bspwm-integration" nil "/usr/bin/bspc" ,@args))
 
 (defun emacs-bspwm-integration (command)
+  "hande a bspc command `command'.
+Currently supported are the following. Bspwm commands are quite intricate. Commands are only supported in their most basic form.
+node -f: focus window in given direction.
+node -s: swap window
+node -p: (preselect and) split window
+node -z: resize window"
   (pcase command
     ((rx bos "node -f")
      (emacs-bspwm-goto-window
@@ -29,6 +36,7 @@
 
 ;; TODO: make into macro
 (defun emacs-bspwm-resize-window (dir dx dy)
+  "Resize the current window towards the direction `dir' by `dx' and `dy' amount in respectively the x and y direction."
   (pcase dir
   ('top_left ((emacs-bspwm-resize-window-straight 'top dx dy)
               (emacs-bspwm-resize-window-straight 'left dx dy)))
@@ -41,6 +49,7 @@
   (- (emacs-bspwm-resize-window-straight dir dx dy))))
 
 (defun emacs-bspwm-resize-window-straight (dir dx dy)
+  "Resize the current window towards direction `dir' by `dx' and `dy' amount. Only resize in a straight line, i.e., never resize the window both over the x and y-axis."
   (let ((target-window
         (pcase dir                      ; The window that must grow/shrink it's right or bottom border
           ('top (windmove-find-other-window 'up))
@@ -63,6 +72,7 @@
 
 
 (defun emacs-bspwm-split-window (dir)
+  "Split the current window in the direction `dir'."
   (pcase dir
     ('west (evil-window-vsplit))
     ;; Also move east to emulate a split to the east (focus the new window)
@@ -74,6 +84,7 @@
             (emacs-bspwm-goto-window 'south))))
 
 (defun convert-bspwm-direction (dir)
+  "Convert a bspwm (polar) direction, i.e. north, west, south, east, to their respective emacs direction, i.e. up, left, down, right."
   (pcase dir
     ('north 'up)
     ('south 'down)
@@ -81,6 +92,7 @@
     ('east 'right)))
 
 (defun emacs-bspwm-goto-window (dir)
+  "Switch focus to the window in direction `dir' of current window."
   (let* ((tdir (convert-bspwm-direction dir))
          (target-window (windmove-find-other-window tdir)))
     (if (or (null target-window) (window-minibuffer-p target-window))
@@ -88,6 +100,7 @@
       (windmove-do-window-select tdir)))
 
 (defun emacs-bspwm-direction-exists-p (dir)
+  "Whether or not the window towards direction `dir' from the current window exists."
   (seq-some (lambda (dir)
               (let ((win (windmove-find-other-window dir)))
                 (and win (not (window-minibuffer-p win)))))
@@ -97,6 +110,7 @@
 
 
 (defun emacs-bspwm-move-window (dir)
+  "Move the current window towards the direction `dir'."
   (let* ((tdir (convert-bspwm-direction dir))
          (target-window (windmove-find-other-window tdir))
          (other-direction (emacs-bspwm-direction-exists-p
